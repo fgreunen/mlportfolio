016e174c958dd234352fb1b31db8c0593fa5f91b
Francois van Greunen <francoisvg@bbd.co.za>
2018-06-20 13:10:51 +0200

    Images

--- Momentum Innovation/Vodacom/Vodacom/SOM.R
+++ Momentum Innovation/Vodacom/Vodacom/SOM.R
@@ -4,20 +4,26 @@ source("_ColourPalettes.R")
 source("_SOM.R")
 bringInRequirements()
 
+OUT_TO_DISK <- TRUE
+
 data <- getData("VOD.csv")
 data <- data[, colnames(data) %in% c("PX_CLOSE_1D",
     "LF_MONTHLY_RETURN", "LF_MONTHLY_VOLATILITY",
     "STYLE_VALUE", "STYLE_QUALITY", "STYLE_GROWTH", "STYLE_MOMENTUM")]
 
 gridSize <- SOMGridSizeRecommendation(nrow(data))
-som.model <- som(as.matrix(data), grid = somgrid(gridSize, gridSize, "hexagonal", toroidal = TRUE))
+som.model <- kohonen::som(as.matrix(data), grid = somgrid(gridSize, gridSize, "hexagonal", toroidal = TRUE), rlen = 100)
 som.hc <- cutree(hclust(object.distances(som.model, "codes")), 2)
 
-# Plot the SOM maps.
-par(mfrow = c(3, 2), mar = c(1, 1, 1, 1))
+if (!OUT_TO_DISK) par(mfrow = c(3, 2), mar = c(1, 1, 1, 1))
+if (OUT_TO_DISK) png(filename = "images/SOM_dist.neighbours.png", width = 8, height = 8, units = "in", res = 400)
 plot(som.model, type = "dist.neighbours", main = "DISTANCE", palette.name = colourPalettes_Blues)
 add.cluster.boundaries(som.model, som.hc)
+if (OUT_TO_DISK) dev.off()
+
 for (i in 1:ncol(data)) {
+    if (OUT_TO_DISK) png(filename = paste("images/SOM_heatmap_", names(data.frame(som.model$data[[1]]))[i], ".png", sep = ""), width = 8, height = 8, units = "in", res = 400)
     plot(som.model, type = "property", property = som.model$codes[[1]][, i], main = names(data.frame(som.model$data[[1]]))[i], palette.name = colourPalettes_Blues)
     add.cluster.boundaries(som.model, som.hc)
+    if (OUT_TO_DISK) dev.off()
 }
--- Momentum Innovation/Vodacom/Vodacom/StylesAcrossTime.R
+++ Momentum Innovation/Vodacom/Vodacom/StylesAcrossTime.R
@@ -4,8 +4,12 @@ source("_ColourPalettes.R")
 source("_SOM.R")
 bringInRequirements()
 
+OUT_TO_DISK <- TRUE
+
 data <- getData("VOD.csv")
 data <- data[order(data$Dates),]
+
+if (OUT_TO_DISK) png(filename = "images/StylesAcrossTime.png", width = 8, height = 8, units = "in", res = 400)
 plot(PX_CLOSE_1D ~ Dates, data, type = "l", ylab = "Normalized Range",
      main = "Styles across time")
 lines(STYLE_VALUE ~ Dates, data, type = "l", col = "red", lwd = 2)
@@ -14,3 +18,4 @@ lines(STYLE_MOMENTUM ~ Dates, data, type = "l", col = "blue", lwd = 2)
 lines(STYLE_GROWTH ~ Dates, data, type = "l", col = "orange", lwd = 2)
 legend(min(data$Dates), 1, c("Value", "Quality", "Momentum", "Growth"),
        col = c("red", "green", "blue", "orange"), lty = c(1, 1, 1, 1), lwd = 2, merge = TRUE, bg = "gray90")
+if (OUT_TO_DISK) dev.off()
